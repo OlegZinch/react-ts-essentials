d94bdbf652f59c71758b33d241b0ed157fe098c9
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-01-06 20:08:09 +0000

    Another Way Of Typing Components

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -9,6 +9,18 @@ type CourseGoalProps = PropsWithChildren<{
   title: string
 }>
 
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   )
+// }
+
 function CourseGoal({ title, children }: CourseGoalProps) {
   return (
     <article>
