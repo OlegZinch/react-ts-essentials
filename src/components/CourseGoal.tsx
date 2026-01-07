69b640cf125f0e08c37ec60edee8d5b82f856bd5
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-01-07 15:58:47 +0000

    Passing Functions as Values - In A Type-Safe Way

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -6,7 +6,9 @@ import type { PropsWithChildren } from 'react'
 // }
 
 type CourseGoalProps = PropsWithChildren<{
+  id: number
   title: string
+  onDelete: (id: number) => void
 }>
 
 // const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
@@ -21,14 +23,14 @@ type CourseGoalProps = PropsWithChildren<{
 //   )
 // }
 
-function CourseGoal({ title, children }: CourseGoalProps) {
+function CourseGoal({ id, title, onDelete, children }: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
         {children}
       </div>
-      <button>Delete</button>
+      <button onClick={() => onDelete(id)}>Delete</button>
     </article>
   )
 }
--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -3,14 +3,15 @@ import { type CourseGoal as CGoal } from '../App.tsx'
 
 type CourseGoalListProps = {
   goals: CGoal[]
+  onDeleteGoal: (id: number) => void
 }
 
-function CourseGoalList({ goals }: CourseGoalListProps) {
+function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
   return (
     <ul>
       {goals.map((goal) => (
         <li key={goal.id}>
-          <CourseGoal title={goal.title}>
+          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
             <p>{goal.description}</p>
           </CourseGoal>
         </li>
