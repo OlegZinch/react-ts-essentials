36c2d578d743a99b4ea704857dedb02ff7a73cfe
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-01-07 17:31:50 +0000

    Working with Generic Event Types

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,19 +1,24 @@
 import type { FormEvent } from 'react'
 
 function NewGoal() {
-  function handleSubmit(event: FormEvent) {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+
+    console.log(formData.get('goal'))
+    console.log(formData.get('summary'))
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor='goal'>Your goal</label>
-        <input id='goal' type='text' />
+        <input id='goal' type='text' name='goal' />
       </p>
       <p>
         <label htmlFor='summary'>Short summary</label>
-        <input id='summary' type='text' />
+        <input id='summary' type='text' name='summary' />
       </p>
       <p>
         <button>Add Goal</button>
