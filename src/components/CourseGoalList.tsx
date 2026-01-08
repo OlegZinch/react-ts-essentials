10fc2375be63e096f3f30d0866ff6c4c04631011
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-01-08 18:49:17 +0000

    Problem: Flexible Components With Required Prop Combinations

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -22,7 +22,7 @@ function CourseGoalList({ goals, onDeleteGoal }: CourseGoalListProps) {
 
   if (goals.length >= 4) {
     warningBox = (
-      <InfoBox mode='warning'>
+      <InfoBox mode='warning' severity='high'>
         You're collecting a lot of goals. Don't put too much on your plate.
       </InfoBox>
     )
